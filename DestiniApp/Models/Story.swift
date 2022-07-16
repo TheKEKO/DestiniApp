2ded5dd1dd9e8ab2fefa59d810101938e471ab5d
Aleksandr F <a7214370@gmail.com>
2022-07-16 19:11:28 +0300

    Setup project

---
+++ DestiniApp/Models/Story.swift
@@ -0,0 +1,8 @@
+//
+//  Story.swift
+//  DestiniApp
+//
+//  Created by Aleksandr F. on 16.07.2022.
+//
+
+import Foundation
---
+++ DestiniApp/Models/StoryBrain.swift
@@ -0,0 +1,8 @@
+//
+//  StoryBrain.swift
+//  DestiniApp
+//
+//  Created by Aleksandr F. on 16.07.2022.
+//
+
+import Foundation
